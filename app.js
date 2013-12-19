654b3cbd110648b3f4a1e7dca90fc532aa949641
Juan Alberto López Cavallotti <juancavallotti@gmail.com>
2013-12-19 15:17:20 -0300

    created separate file the core functionlity

--- app.js
+++ app.js
@@ -4,9 +4,10 @@
  */
 
 var express = require('express');
-var raml = require('raml-parser');
 var http = require('http');
 var path = require('path');
+
+var apikit = require('./lib/apikit.js');
 
 var app = express();
 
@@ -15,7 +16,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
-app.use(express.express('dev'));
+//app.use(express.express('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
@@ -29,26 +30,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-raml.loadFile('raml/regression.yml').then( function(data) {
-    var apiResources = data.resources;
-    console.log(data.resources[0].methods);
-
-    for (var i=0;i<apiResources.length;i++) {
-        for (var j=0;j<apiResources[i].methods.length;j++) {
-            app[apiResources[i].methods[j].method](apiResources[i].relativeUri,
-                require('./routes' + apiResources[i].relativeUri)[apiResources[i].methods[j].method]);
-            console.log(apiResources[i].methods[j].method);
-        }
-
-        console.log(apiResources[i].relativeUri);
-    }
-
-    http.createServer(app).listen(app.get('port'), function(){
-        console.log('Express server listening on port ' + app.get('port'));
+var api = new apikit('./raml/regression.yml', app, function(configuredApp){
+    http.createServer(configuredApp).listen(configuredApp.get('port'), function(){
+        console.log('Express server listening on port ' + configuredApp.get('port'));
     });
-}, function(error) {
-    console.log('Error parsing: ' + error);
 });
-
-
-
